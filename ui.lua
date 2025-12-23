8c2357fa8d52e2da4c006127f6e48954673b46a7
ApelsinkaFr <105067000+ApelsinkaFr@users.noreply.github.com>
2025-12-23 22:33:32 +1000

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1867,7 +1867,7 @@ function MacLib:Window(Settings)
 
 					local sliderUIListLayout = Instance.new("UIListLayout")
 					sliderUIListLayout.Name = "SliderUIListLayout"
-					sliderUIListLayout.Padding = UDim.new(0, 8)
+					sliderUIListLayout.Padding = UDim.new(0, 1)
 					sliderUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
 					sliderUIListLayout.Parent = slider
 
